b97bb4f100f9ace50c3a3693a7479e57389fa81d
Pat Cunha <114702163+xic-let@users.noreply.github.com>
2023-05-19 15:49:31 +0100

    initial

---
+++ Concelho.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using namespace Saude_DIstante;
+
+
+public enum  Concelho:Utente
+{
+		Braga,
+		Porto, 
+		Coimbra,
+		Lisboa,
+		Portimão
+
+}
---
+++ Utente.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Saude_DIstante
+{
+    public class Utente:User
+    {
+        public int Idsns;
+        public enum Concelho;
+        public bool Rgpd;
+
+        list <DadosSaude> dadoutente = new.DadosSaude();
+
+        public Utente() { }
+
+        public Utente(int idsns, enum concelho, bool rgpd)
+        {
+            this.Idsns = idsns;
+            this.Concelho = concelho;
+            this.Rgpd = rgpd;
+        }
+
+    }
+}
