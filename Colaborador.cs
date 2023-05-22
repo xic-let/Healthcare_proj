0cba2be0590e8c8b2161dc0b1e350ff21cc35fae
NunoJDMachado <njdmnuno7@hotmail.com>
2023-05-22 10:50:27 +0100

    adicionei classe colaborador

---
+++ Colaborador.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Colaborador : User
+{
+	public double Vencimento;
+	public char Equipa;
+	public string Viatura;
+
+	
+	public Colaborador()
+	{
+
+	}
+
+	public Colaborador(double vencimento, char equipa, string viatura)
+	{
+		Vencimento = vencimento;
+		Equipa = equipa;
+		Viatura = viatura;
+	}
+}
