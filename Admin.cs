bc2e92d63beb3122a4567ff51bf9495cf2eb3cec
NunoJDMachado <njdmnuno7@hotmail.com>
2023-05-24 14:09:14 +0100

    classes Admin e ItemInventario

---
+++ Admin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Admin : Colaborador
+{
+    public List<ItemInventario> inventario = new List<ItemInventario>();
+
+    
+    public Admin()
+    {
+
+    }
+
+    public Admin(double vencimento, char equipa, string viatura)
+        :base(vencimento, equipa, viatura)
+    {
+
+    }
+
+    public void AddItemInventario(ItemInventario item)
+    {
+        inventario.Add(item);
+    }
+}
---
+++ ItemInventario.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class ItemInventario
+{
+	public string Produto;
+	public int Quantidade;
+	public double Preco;
+	
+	
+	public ItemInventario()
+	{
+
+	}
+
+    public ItemInventario(string produto, int quantidade, double preco)
+    {
+		Produto = produto;
+		Quantidade = quantidade;
+		Preco = preco;
+    }
+
+    
+	public double Total()
+    {
+        return Quantidade * Preco;
+    }
+
+
+}
