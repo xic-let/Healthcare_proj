869c2112192137397ad0a9bf348996ef7eb9a93b
Helena <118369974+Lenavaz@users.noreply.github.com>
2023-06-06 17:40:33 +0100

    correçao erros

--- saude-distante login/saude-distante login/Entities/Enfermeiro.cs
+++ saude-distante login/saude-distante login/Entities/Enfermeiro.cs
@@ -16,7 +16,7 @@ namespace saude_distante_login.Entities
         //Construtores da classe Enfermeiro
         //public Enfermeiro(){ }
 
-        public Enfermeiro(string nome, Genero genero, DateTime dataNascimento, string contacto, string morada, Concelho concelho, string email, int idColaborador, string funcao, double vencimento, Equipa equipa, string password)
+        public Enfermeiro(string nome, Genero genero, DateTime dataNascimento, string contacto, string morada, Concelho concelho, string email, int idColaborador, string funcao, double vencimento, int equipa, string password)
             : base(nome, genero, dataNascimento, contacto, morada, concelho, email, idColaborador, funcao, vencimento, equipa, password)
         {
             
